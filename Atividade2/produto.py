4bba9d6c599fed387d9344e394045df64ca1ab3b
Nicolau Almeida <nicolasalmeidaalves@hotmail.com>
2023-11-08 19:48:21 -0300

    Criacao da Classe Produto

---
+++ Atividade2/produto.py
@@ -0,0 +1,7 @@
+class Produto:
+    def __init__(self, nome, descricao, data_fabricacao, ativo, categoria):
+        self.nome = nome
+        self.descricao = descricao
+        self.data_fabricacao = data_fabricacao
+        self.ativo = ativo
+        self.categoria = categoria
