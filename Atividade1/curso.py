54f21f4cba170c98494ae65c1f3f0fb7759dc5ee
Nicolau Almeida <nicolasalmeidaalves@hotmail.com>
2023-11-09 21:31:38 -0300

    Atualizando Classe Curso

--- Atividade1/curso.py
+++ Atividade1/curso.py
@@ -1,7 +1,7 @@
 class Curso:
-    def __init__(self, titulo, duracao):
+    def __init__(self, titulo, carga_horaria):
         self.titulo = titulo
-        self.duracao = duracao
+        self.carga_horaria = carga_horaria
 
     def __str__(self):
-        return f"{self.titulo} ({self.duracao})"
+        return f"{self.titulo} ({self.carga_horaria})"
