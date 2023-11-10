e3014ab07c54ae3d5846bcfdc7697c3f3013efaa
Nicolau Almeida <nicolasalmeidaalves@hotmail.com>
2023-11-09 21:46:55 -0300

    Criacao da Classe Matricula

---
+++ Atividade1/matricula.py
@@ -0,0 +1,7 @@
+class Matricula:
+    def __init__(self, numero_matricula, data_matricula, curso, aluno, contratante):
+        self.numero_matricula = numero_matricula
+        self.data_matricula = data_matricula
+        self.curso = curso
+        self.aluno = aluno
+        self.contratante = contratante
