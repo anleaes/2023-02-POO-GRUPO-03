3bc81d7686725bc2efcb367c4d0f4f16f6231fc1
Nicolau Almeida <nicolasalmeidaalves@hotmail.com>
2023-11-09 21:53:20 -0300

    Atualizando Classe Inscrição

--- Atividade1/incricao.py
+++ Atividade1/incricao.py
@@ -1,3 +1,7 @@
 class Inscricao:
-    def realizar_inscricao(self, pessoa, curso):
-        print(f"{pessoa.nome} se inscreveu no curso de {curso.titulo}")
+    def __init__(self, valor_inscricao, data_matricula, curso, aluno_matriculado, contratante):
+        self.valor_inscricao = valor_inscricao
+        self.data_matricua = data_matricula
+        self.curso = curso
+        self.aluno_matriculado = aluno_matriculado
+        self.contratante = contratante
