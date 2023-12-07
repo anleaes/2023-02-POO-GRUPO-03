baaebb977c80db68c03cb89c85bf9334e9577f14
Felipe.Bourqui <felipe.bourqui@compuletra.com.br>
2023-12-07 17:30:57 -0300

    Implementado Classe Profissional

--- apps/profissional/models.py
+++ apps/profissional/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Profissional(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    habilidades = models.JSONField()
+    servicosPrestados = models.ManyToManyField(Servico)
