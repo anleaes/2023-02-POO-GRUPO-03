bbcd6cca33bd051ba08ca1afcab1f515012c8005
Gabriel Luz <gabrielmfluz@gmail.com>
2023-12-07 19:26:23 -0300

    Atualização models

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class cliente(models.models):
+class cliente(models.Model):
     nome = models.CharField(max_length=300)
     idade = models.IntegerField()
     cpf_cliente = models.CharField(max_length=50)
--- apps/formapagamento/models.py
+++ apps/formapagamento/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class formapagamento(models.models):
+class formapagamento(models.Model):
     banco = models.CharField(max_length=100)
     tipo_pagamento = models.CharField(max_length=100)
     recibo = models.CharField(max_length=100)
