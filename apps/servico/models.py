120a7b451063ee11f27418ba3f88f8bea2007f51
antoniolopesacml <antoniolopesacml@gmail.com>
2023-12-07 17:37:23 -0300

    Implementado atributos da classe serviço

--- apps/servico/models.py
+++ apps/servico/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Servico(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100)
-     
+
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+    tempo_estimado = models.IntegerField('Tempo Estimado (minutos)')
+    categoria = models.CharField('Categoria', max_length=50)
+    
     class Meta:
         verbose_name = 'Servico Prestado'
         verbose_name_plural = 'Servicos Prestados'
