6876332a063e5f09bbd5bbad1c846a873a1e2d60
Gabriel Luz <gabrielmfluz@gmail.com>
2023-12-07 18:07:04 -0300

    Alteração cpf_cliente

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class cliente(models.models):
     nome = models.CharField(max_length=300)
     idade = models.IntegerField()
-    cpf = models.CharField(max_length=50)
+    cpf_cliente = models.CharField(max_length=50)
