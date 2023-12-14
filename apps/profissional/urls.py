a44b8bcede59a60f7885111cfc4d38ad3f3ffada
Felipe.Bourqui <felipe.bourqui@compuletra.com.br>
2023-12-14 18:16:17 -0300

    feito todas as alterações do profissional

--- apps/profissional/urls.py
+++ apps/profissional/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'Profissional'
+#app_name = 'Profissional'
+app_name = 'profissional'
 
 router = routers.DefaultRouter()
-router.register('', views.ProfissionalViewSet, basename='Profissional')
+#router.register('', views.ProfissionalViewSet, basename='Profissional')
+router.register('', views.ProfissionalViewSet, basename='profissionais')
 
 urlpatterns = [
     path('', include(router.urls) )
