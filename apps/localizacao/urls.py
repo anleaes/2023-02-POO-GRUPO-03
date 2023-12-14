6df9f906dd64bcbd09fbe0fa6de5522e6b061b88
Felipe.Bourqui <felipe.bourqui@compuletra.com.br>
2023-12-14 18:17:46 -0300

    feito todas alterações necessárias no localização

--- apps/localizacao/urls.py
+++ apps/localizacao/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'Localizacao'
+#app_name = 'Localizacao'
+app_name = 'localizacao'
 
 router = routers.DefaultRouter()
-router.register('', views.LocalizacaoViewSet, basename='Localizacao')
+#router.register('', views.LocalizacaoViewSet, basename='Localizacao')
+router.register('', views.LocalizacaoViewSet, basename='localizacoes')
 
 urlpatterns = [
     path('', include(router.urls) )
