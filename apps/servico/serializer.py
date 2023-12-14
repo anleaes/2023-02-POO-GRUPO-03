59faecb06ac665796d415ffec1fd2f17b0ad936c
antoniolopesacml <antoniolopesacml@gmail.com>
2023-12-14 18:46:13 -0300

    Alteração final

--- apps/servico/serializer.py
+++ apps/servico/serializer.py
@@ -5,9 +5,3 @@ class ServicoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servico
         fields = '__all__'
-        
-        # Para chamar todos os atributos:
-        # fields = '__all__'
-        
-        # Para chamar somentes os atributos de interesse:
-        # fields = ['id','created_on', 'updated_on', 'name', 'description']
